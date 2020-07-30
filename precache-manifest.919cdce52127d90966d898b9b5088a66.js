5f52ce7698f3c3e3ac67364d60c6f5a65c0aa178
naseebasoftfruit <naseebasoftfruit@gmail.com>
2020-07-30 14:33:01 +0530

    Updates

---
+++ precache-manifest.919cdce52127d90966d898b9b5088a66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c04f244562a42f7b83371dbc96a7860e",
+    "url": "/click-count/index.html"
+  },
+  {
+    "revision": "ba2b97fc4fcdeb8db411",
+    "url": "/click-count/static/css/main.c0b6f613.chunk.css"
+  },
+  {
+    "revision": "b8db3b5ccb6fbab6a40a",
+    "url": "/click-count/static/js/2.0257cfe2.chunk.js"
+  },
+  {
+    "revision": "ba2b97fc4fcdeb8db411",
+    "url": "/click-count/static/js/main.87ff1a47.chunk.js"
+  },
+  {
+    "revision": "c5e03f047252a569fa81",
+    "url": "/click-count/static/js/runtime-main.22e7e41b.js"
+  }
+]);
